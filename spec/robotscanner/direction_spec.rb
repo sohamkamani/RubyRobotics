56619a16d6974715960752cc5e44da2dc78b8304
Soham Chetan <sohamchetan@Sohams-MacBook-Pro.local>
2015-02-05 10:56:09 +0530

    added functionality for turning 360

--- spec/robotscanner/direction_spec.rb
+++ spec/robotscanner/direction_spec.rb
@@ -1,21 +1,21 @@
 require "spec_helper"
 
 describe "Direction" do
-	let(:direction1) { Direction.east}
+  let(:direction1) { Direction.east}
 
-	it "should turn left" do
-		expect(direction1.left).to eq(Direction.north)
-	end
+  it "should turn left" do
+    expect(direction1.left).to eq(Direction.north)
+  end
 
-	it "should turn left twice and thus face west" do
-		direction1.left
-    expect(direction1.left).to eq(Direction.west)		
-	end
+  it "should turn left twice and thus face west" do
+    direction1.left
+    expect(direction1.left).to eq(Direction.west)
+  end
 
   it "should turn left thrice and thus face south" do
     direction1.left
     direction1.left
-    expect(direction1.left).to eq(Direction.south)   
+    expect(direction1.left).to eq(Direction.south)
   end
 
   it "should turn right once, thus facing south" do
@@ -24,12 +24,19 @@ describe "Direction" do
 
   it "should turn right twice and thus face west" do
     direction1.right
-    expect(direction1.right).to eq(Direction.west)   
+    expect(direction1.right).to eq(Direction.west)
   end
 
   it "should turn right thrice and thus face north" do
     direction1.right
     direction1.right
-    expect(direction1.right).to eq(Direction.north)   
+    expect(direction1.right).to eq(Direction.north)
+  end
+
+  it "should turn left four times and thus return to original position" do
+    direction1.left
+    direction1.left
+    direction1.left
+    expect(direction1.left).to eq(Direction.east)
   end
 end
