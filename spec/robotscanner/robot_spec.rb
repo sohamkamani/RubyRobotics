beb62bc62e248a3c0250ddb3530156191d580058
Soham Chetan <sohamchetan@Sohams-MacBook-Pro.local>
2015-02-05 11:39:01 +0530

    tested robot class for equality

--- spec/robotscanner/robot_spec.rb
+++ spec/robotscanner/robot_spec.rb
@@ -2,7 +2,8 @@ require "spec_helper"
 
 describe "Robot" do
   let(:robot1) {Robot.new(Position.new(5,5),Direction.north)}
-  let(:robot2) {Robot.new(Position.new(5,5),Direction.east)}
+  let(:robot2) {Robot.new(Position.new(5,5),Direction.north)}
+  let(:robot3) {Robot.new(Position.new(5,5),Direction.north)}
 
   it "should be able to turn right" do
     expect(robot1.turn_right).to eq(Robot.new(Position.new(5,5),Direction.east))
@@ -13,7 +14,21 @@ describe "Robot" do
   end
 
   it "should be able to move" do
-    expect(robot1.turn_left).to eq(Robot.new(Position.new(5,5),Direction.west))
+    expect(robot1.move).to eq(Robot.new(Position.new(5,6),Direction.north))
+  end
+
+  describe 'Equality' do
+    it "follows reflexivity" do
+      expect(robot1).to eq(robot1)
+    end
+
+    it "follows symmetry" do
+      expect(robot1==robot2).to eq(robot2==robot1)
+    end
+
+    it "follows transitivity" do
+      expect(robot1==robot2 && robot2==robot3).to eq(robot1==robot3)
+    end
   end
 
 end
