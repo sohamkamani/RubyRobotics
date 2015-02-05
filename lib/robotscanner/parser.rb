574d635063b141e739e03452ae50cb65c29f589e
Soham Chetan <sohamchetan@Sohams-MacBook-Pro.local>
2015-02-05 12:08:33 +0530

    added parsing for command strings

--- lib/robotscanner/parser.rb
+++ lib/robotscanner/parser.rb
@@ -4,6 +4,34 @@ class Parser
     coordinates = command_string.split(" ")
     Position.new(coordinates[0].to_i,coordinates[1].to_i)
   end
-  
-  
+
+  def parse_robot(command_string)
+    robot_props = command_string.split(" ")
+    if(robot_props[2]=="NORTH")
+      robot = Robot.new(Position.new(robot_props[0].to_i,robot_props[1].to_i),Direction.north)
+    elsif robot_props[2] == "SOUTH"
+      robot = Robot.new(Position.new(robot_props[0].to_i,robot_props[1].to_i),Direction.south)
+    elsif robot_props[2] == "EAST"
+      robot = Robot.new(Position.new(robot_props[0].to_i,robot_props[1].to_i),Direction.east)
+    elsif robot_props[2] == "WEST"
+      robot = Robot.new(Position.new(robot_props[0].to_i,robot_props[1].to_i),Direction.west)
+    end
+    return robot
+  end
+
+  def parse_commands(command_string)
+    command_collection = []
+    commands = command_string.split
+    commands.each do |command|
+      if command == "MOVE"
+        command_collection.push(MoveCommand.new)
+      elsif command == "RIGHT"
+        command_collection.push(RightCommand.new)
+      elsif command == "LEFT"
+        command_collection.push(LeftCommand.new)
+      end
+    end
+    return command_collection
+  end
+
 end
--- spec/robotscanner/parser_spec.rb
+++ spec/robotscanner/parser_spec.rb
@@ -5,4 +5,13 @@ describe "parser" do
   it "should parse boundary position" do
     expect(parser.parse_position("5 5")).to eq(Position.new(5,5))
   end
+
+  it "should parse a robot" do
+    expect(parser.parse_robot("1 2 NORTH")).to eq(Robot.new(Position.new(1,2),Direction.north))
+  end
+
+  it "should parse a series of commands" do
+    commands = parser.parse_commands("MOVE LEFT MOVE")
+    expect([commands[0].class,commands[1].class,commands[2].class]).to eq([MoveCommand,LeftCommand,MoveCommand])
+  end
 end
