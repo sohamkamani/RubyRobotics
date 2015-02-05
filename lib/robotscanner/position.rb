c8c0454be1958a15a97031caa2d8cda0e06323bf
Soham Chetan <sohamchetan@Sohams-MacBook-Pro.local>
2015-02-05 11:10:03 +0530

    added position movement for east

--- lib/robotscanner/position.rb
+++ lib/robotscanner/position.rb
@@ -1,21 +1,24 @@
 class Position
 
   attr_reader :x,:y
-  
+
   def initialize(x,y)
     @x = x
     @y = y
   end
 
   def move(direction)
-    @y +=1
-
+    if direction == Direction.north
+      @y +=1
+    elsif direction == Direction.east
+      @x +=1
+    end
     return Position.new(@x,@y)
   end
 
   def ==(other)
     (@x == other.x && @y == other.y)
   end
-  
-  
+
+
 end
--- spec/robotscanner/position_spec.rb
+++ spec/robotscanner/position_spec.rb
@@ -3,9 +3,12 @@ require "spec_helper"
 describe "Position" do
   let(:position1) {Position.new(0,0)}
   let(:position2) {Position.new(0,1)}
-  let(:direction) {Direction.north}
 
-  it "should change in the required direction" do
-    expect(position1.move(direction)).to eq(position2)
+  it "should change in the required direction , north" do
+    expect(position1.move(Direction.north)).to eq(position2)
+  end
+
+  it "should change in the required direction , east" do
+    expect(position1.move(Direction.east)).to eq(Position.new(1,0))
   end
 end
